293d95efc6f8a07e706068981b2562e33d09455f
Benjamin Bannier <bbannier@gmail.com>
2016-08-22 09:51:12 +0200

    Adjust header filter expression to also show errors from Mesos includes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ echo '############################## BUILDING SOURCES ##########################
 bear -- make -j$(nproc) check GTEST_FILTER=''
 
 echo '############################## CHECKING SOURCES ##############################'
-TIDY_ARGS="--extra-arg=-Wno-unused-command-line-argument -header-filter=$SRCDIR'/(src|3rdparty/stout|3rdparty/libprocess).*'"
+TIDY_ARGS="--extra-arg=-Wno-unused-command-line-argument -header-filter=$SRCDIR'/(include|src|3rdparty/stout|3rdparty/libprocess).*'"
 DEFAULT_CHECKS='-*,mesos-*'
 CHECKS=${CHECKS:-$DEFAULT_CHECKS}
 

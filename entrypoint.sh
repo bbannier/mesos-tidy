3e7c7173d4d04133315dba82591a0d10504c95ba
Benjamin Bannier <bbannier@gmail.com>
2016-08-30 14:01:32 +0200

    Fixed header filter expression.

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ echo '############################## BUILDING SOURCES ##########################
 bear -- make -j$(nproc) check GTEST_FILTER=''
 
 echo '############################## CHECKING SOURCES ##############################'
-TIDY_ARGS="--extra-arg=-Wno-unused-command-line-argument -header-filter=$SRCDIR'/(include|src|3rdparty/stout|3rdparty/libprocess).*'"
+TIDY_ARGS="--extra-arg=-Wno-unused-command-line-argument -header-filter=$SRCDIR'/.*'"
 DEFAULT_CHECKS='-*,mesos-*'
 CHECKS=${CHECKS:-$DEFAULT_CHECKS}
 
@@ -22,5 +22,5 @@ cat compile_commands.json \
 	| sed 's/"//g' \
 	| sed 's/^\ //g' \
 	| grep '^'${SRCDIR} \
-	| parallel -j $(nproc) clang-tidy -p $PWD "${TIDY_ARGS}" -checks="${CHECKS}" \
+	| parallel -j $(nproc) clang-tidy -p "${PWD}" "${TIDY_ARGS}" -checks="${CHECKS}" \
   1> /OUT/clang-tidy.log 2> /dev/null
